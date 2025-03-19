7ea09a7c66e5305f42fa6b86f1ef9b182ea2c1c8
Jeeva.M <142040374+Jeevanandham4203@users.noreply.github.com>
2025-03-19 21:57:40 +0530

    Delete Employee directory

--- Employee/src/main/java/com/employee/CRUD/Repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.employee.CRUD.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.employee.CRUD.Entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
-	
-}
